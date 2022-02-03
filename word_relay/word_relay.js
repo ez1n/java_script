740c480735ce56757a6577d4725896dcdc9fe8e3
unknown <eodrmfdl1004@naver.com>
2022-02-03 14:05:17 +0900

    update word_relay game

--- word_relay/word_relay.js
+++ word_relay/word_relay.js
@@ -1,4 +1,4 @@
-const num = Number(prompt('몇 명이 참가하나요?'));
+const num = Number(prompt('참가인원'));
 const $input = document.querySelector('input');
 const $button = document.querySelector('button');
 const $word = document.querySelector('#word');
@@ -10,18 +10,24 @@ const click_btn = () => {
   if(!word) { //empty
     word = new_word; //입력 단어 -> 제시어
     $word.textContent = word; //화면에 제시어 표시
+    $input.value = ''; //입력창 비우기
+    $input.focus(); //입력창 내부에 커서 위치
   } else {//full
     if(word[word.length - 1] == new_word[0]) { //same
       word = new_word;
       $word.textContent = word;
       const order = Number($order.textContent);
-      if (order + 1 > num) { //마지막 순서
+      if (order + 1 > num) { //last
         $order.textContent = 1;
-      } else { //마지막 순서 x
+      } else { //non-last
         $order.textContent = order + 1;
       }
+      $input.value = '';
+      $input.focus();
     } else{ //different
-
+      alert('fail!');
+      $input.value = '';
+      $input.focus();
     }
   }
 };
