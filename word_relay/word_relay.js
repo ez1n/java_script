c1d33be59ffaa04295a0662d76e624c07fbf8ab4
unknown <eodrmfdl1004@naver.com>
2022-02-03 14:56:22 +0900

    update word relay game

--- word_relay/word_relay.js
+++ word_relay/word_relay.js
@@ -5,12 +5,16 @@ const $word = document.querySelector('#word');
 const $order = document.querySelector('#order')
 let word; //제시어
 let new_word; //현재 단어
+$input.focus();
 
+//버튼 클릭 이벤트
 const click_btn = () => {
+  //제시어가 비어있거나 올바른가?
   if (!word || word[word.length - 1] == new_word[0]) { //empty or correct
     word = new_word; //입력 단어 -> 제시어
     $word.textContent = word; //화면에 제시어 표시
 
+    //순서 넘기기
     const order = Number($order.textContent);
     if (order + 1 > num) { //last
       $order.textContent = 1;
@@ -24,9 +28,18 @@ const click_btn = () => {
   $input.value = ''; //입력창 비우기
   $input.focus(); //입력창 내부에 커서 위치
 };
+
+// 엔터 키 이벤트
+const enter_press = (event) => {
+  if (event.keyCode === 13) {
+   click_btn()
+  }
+};
+
 const on_input = (event) => {
   new_word = event.target.value; //입력 단어 -> 현재 단어로 변경
 };
 
 $input.addEventListener('input', on_input);
+$input.addEventListener('keydown', enter_press);
 $button.addEventListener('click', click_btn);
