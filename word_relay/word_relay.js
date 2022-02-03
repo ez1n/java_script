8c4183d610473634eb144c916699fa4c02d015c1
unknown <eodrmfdl1004@naver.com>
2022-02-03 14:16:04 +0900

    update word relay game

--- word_relay/word_relay.js
+++ word_relay/word_relay.js
@@ -7,29 +7,22 @@ let word; //제시어
 let new_word; //현재 단어
 
 const click_btn = () => {
-  if(!word) { //empty
+  if (!word || word[word.length - 1] == new_word[0]) { //empty or correct
     word = new_word; //입력 단어 -> 제시어
     $word.textContent = word; //화면에 제시어 표시
-    $input.value = ''; //입력창 비우기
-    $input.focus(); //입력창 내부에 커서 위치
-  } else {//full
-    if(word[word.length - 1] == new_word[0]) { //same
-      word = new_word;
-      $word.textContent = word;
-      const order = Number($order.textContent);
-      if (order + 1 > num) { //last
-        $order.textContent = 1;
-      } else { //non-last
-        $order.textContent = order + 1;
-      }
-      $input.value = '';
-      $input.focus();
-    } else{ //different
-      alert('fail!');
-      $input.value = '';
-      $input.focus();
+
+    const order = Number($order.textContent);
+    if (order + 1 > num) { //last
+      $order.textContent = 1;
+    } else { //non-last
+      $order.textContent = order + 1;
     }
+
+  } else {//full and not correct
+    alert('fail!');
   }
+  $input.value = ''; //입력창 비우기
+  $input.focus(); //입력창 내부에 커서 위치
 };
 const on_input = (event) => {
   new_word = event.target.value; //입력 단어 -> 현재 단어로 변경
