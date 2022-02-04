6678947f5a28347f32faf2f6c276c85fc5415612
unknown <eodrmfdl1004@naver.com>
2022-02-04 22:46:30 +0900

    Exclude duplicate words

--- word_relay/index.js
+++ word_relay/index.js
@@ -5,37 +5,44 @@ const $word = document.querySelector('#word');
 const $order = document.querySelector('#order')
 let word; //제시어
 let new_word; //현재 단어
+let old_word = new Array(); //지금까지 제시된 단어
 $input.focus();
 
 //버튼 클릭 이벤트
 const click_btn = () => {
   //제시어가 비어있거나 올바른가?
   if (!word || word[word.length - 1] == new_word[0]) { //empty or correct
-    word = new_word; //입력 단어 -> 제시어
-    $word.textContent = word; //화면에 제시어 표시
 
-    //순서 넘기기
-    const order = Number($order.textContent);
-    if (order + 1 > num) { //last
-      $order.textContent = 1;
-    } else { //non-last
-      $order.textContent = order + 1;
+    //제시어가 중복되지 않는가?
+    if (!old_word.includes(new_word)) {
+      word = new_word; //입력 단어 -> 제시어
+      $word.textContent = word; //화면에 제시어 표시
+      old_word.push(new_word)
+
+      //순서 넘기기
+      const order = Number($order.textContent);
+      if (order + 1 > num) { //last
+        $order.textContent = 1;
+      } else { //non-last
+        $order.textContent = order + 1;
+      }
+      const $result = document.querySelector('#result');
+      $result.innerHTML += $word.textContent + '&nbsp;';
+      
+    } else {
+      alert('올바르지 않은 단어입니다!');
     }
-
   } else {//full and not correct
     alert('올바르지 않은 단어입니다!');
   }
   $input.value = ''; //입력창 비우기
   $input.focus(); //입력창 내부에 커서 위치
-
-  const $result = document.querySelector('#result'); 
-  $result.innerHTML += $word.textContent + '&nbsp;';
 };
 
 // 엔터 키 이벤트
 const enter_press = (event) => {
   if (event.keyCode === 13) {
-   click_btn()
+    click_btn()
   }
 };
 
