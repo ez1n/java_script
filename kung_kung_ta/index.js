6919fa407765857b695e3f66735c97aebf77d16c
unknown <eodrmfdl1004@naver.com>
2022-02-04 14:28:57 +0900

    update kung_kung_ta game

---
+++ kung_kung_ta/index.js
@@ -0,0 +1,9 @@
+const num = document.querySelector('#num');
+const word = document.querySelector('#word');
+const input_box = document.querySelector('input');
+const btn = document.querySelector('button');
+const used_word = document.querySelector('#used_word');
+
+const real_num = Number(prompt('참가 인원', 1));//인원 정하기
+input_box.focus(); //커서 놓기
+
