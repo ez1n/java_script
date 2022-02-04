1d0f00a54b4f52c7d45957cfaa8ad65bb31e0fc9
unknown <eodrmfdl1004@naver.com>
2022-02-04 18:28:40 +0900

    중복되는 단어 제외 ing

--- kung_kung_ta/index.js
+++ kung_kung_ta/index.js
@@ -3,8 +3,9 @@ const word = document.querySelector('#word');
 const input_box = document.querySelector('input');
 const btn = document.querySelector('button');
 const used_word = document.querySelector('#used_word');
-let origin_word; //기존 단어
-let new_word; //현재 단어
+let last_word; //마지막 단어
+let new_word; //입력된 단어
+let origin_words = new Array(); //지금까지 나온 단어들
 
 const real_num = Number(prompt('참가 인원', 1)); //인원 정하기
 input_box.focus(); //커서 놓기
@@ -12,11 +13,11 @@ input_box.focus(); //커서 놓기
 //button event
 const on_btn = () => {
   //제시어가 비어있거나 올바른가?
-  if (!origin_word || origin_word[origin_word.length - 1] == new_word[0]) { //empty
+  if (!last_word || last_word[last_word.length - 1] == new_word[0]) { //empty
 
     if (new_word.length == 3) { //단어길이==3
       word.textContent = new_word;
-      origin_word = new_word; //제시어 -> 현재단어
+      last_word = new_word; //제시어 -> 현재단어
       used_word.innerHTML += new_word + '  쿵쿵따  ♥ ';
 
       //순서 넘김
@@ -34,6 +35,7 @@ const on_btn = () => {
   } else { //full
     alert('다시 입력해 주세요!');
   }
+  origin_words.push(new_word)
   input_box.value = '';
   input_box.focus();
 };
