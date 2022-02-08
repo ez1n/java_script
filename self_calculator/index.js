4373c525addd64133d966060ba02c20360f7ead7
unknown <eodrmfdl1004@naver.com>
2022-02-08 22:32:20 +0900

    clean up the code

--- self_calculator/index.js
+++ self_calculator/index.js
@@ -1,21 +1,9 @@
 const result = document.querySelector('#result');
-const num0 = document.querySelector('#num0');
-const num1 = document.querySelector('#num1');
-const num2 = document.querySelector('#num2');
-const num3 = document.querySelector('#num3');
-const num4 = document.querySelector('#num4');
-const num5 = document.querySelector('#num5');
-const num6 = document.querySelector('#num6');
-const num7 = document.querySelector('#num7');
-const num8 = document.querySelector('#num8');
-const num9 = document.querySelector('#num9');
+const num_btns = document.querySelectorAll('#num');
+const operator_btns = document.querySelectorAll('#operator');
 const reset = document.querySelector('#reset');
 const sign = document.querySelector('#sign');
 const percentage = document.querySelector('#percentage');
-const divide = document.querySelector('#divide');
-const multiply = document.querySelector('#multiply');
-const plus = document.querySelector('#plus');
-const minus = document.querySelector('#minus');
 const calculate = document.querySelector('#calculate');
 const decimal_point = document.querySelector('#decimal_point');
 let number1 = '';
@@ -23,6 +11,7 @@ let number2 = '';
 let operator = '';
 
 //alert
+const alert_param = { icon: 'warning', title: '숫자를 입력해 주세요!' };
 const warning_notice = Swal.mixin({
   toast: true,
   position: 'center-center',
@@ -57,21 +46,12 @@ const click_num = (event) => {
     result.value = '0';
     return;
   }
-  
+
   if (!number2) {
     result.value = '';
   }
   number2 += event.target.textContent;
   result.value += event.target.textContent;
-};
-
-//reset btn event
-const click_reset = () => {
-  number1 = '';
-  number2 = '';
-  operator = '';
-  result.value = '';
-  reset.textContent = 'AC';
 };
 
 //decimal_point btn event
@@ -125,8 +105,6 @@ const click_calculate = () => {
       case '+':
         result.value = Number(number1) + Number(number2);
         break;
-
-      default: break;
     }
     number1 = result.value;
     number2 = '';
@@ -134,10 +112,7 @@ const click_calculate = () => {
   } else {
     number1 = '';
     result.value = '';
-    warning_notice.fire({
-      icon: 'warning',
-      title: '숫자를 입력해 주세요!'
-    });
+    warning_notice.fire(alert_param);
   }
 };
 
@@ -153,15 +128,33 @@ const click_operator = (event) => {
       operator = event.target.textContent;
     }
   } else {
-    warning_notice.fire({
-      icon: 'warning',
-      title: '숫자를 입력해 주세요!'
-    });
+    warning_notice.fire(alert_param);
   }
 };
 
-//sign btn event
-const click_sign = () => {
+//add event
+for (let num_btn of num_btns) {
+  num_btn.addEventListener('click', click_num);
+};
+for (let operator_btn of operator_btns) {
+  operator_btn.addEventListener('click', click_operator);
+};
+
+percentage.addEventListener('click', () => {
+  result.value /= 100;
+  number1 = result.value;
+  number2 = '';
+});
+
+reset.addEventListener('click', () => {
+  number1 = '';
+  number2 = '';
+  operator = '';
+  result.value = '';
+  reset.textContent = 'AC';
+});
+
+sign.addEventListener('click', () => {
   if (!number1 || result.value == '0') {
     number1 += '-0';
     result.value = '-0';
@@ -170,34 +163,8 @@ const click_sign = () => {
     result.value = number1;
     number2 = ''
   }
-};
+});
 
-//percentage btn event
-const click_percentage = () => {
-  result.value /= 100;
-  number1 = result.value;
-  number2 = '';
-};
-
-num0.addEventListener('click', click_num);
-num1.addEventListener('click', click_num);
-num2.addEventListener('click', click_num);
-num3.addEventListener('click', click_num);
-num4.addEventListener('click', click_num);
-num5.addEventListener('click', click_num);
-num6.addEventListener('click', click_num);
-num7.addEventListener('click', click_num);
-num8.addEventListener('click', click_num);
-num9.addEventListener('click', click_num);
-percentage.addEventListener('click', click_percentage);
-divide.addEventListener('click', click_operator);
-multiply.addEventListener('click', click_operator);
-minus.addEventListener('click', click_operator);
-plus.addEventListener('click', click_operator);
-reset.addEventListener('click', click_reset);
-sign.addEventListener('click', click_sign);
 calculate.addEventListener('click', click_calculate);
 decimal_point.addEventListener('click', click_decimal_point);
-
-
 
