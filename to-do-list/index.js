97b722fa0e540ea26efe15d97d6f19d15d104d27
unknown <eodrmfdl1004@naver.com>
2022-02-28 14:55:58 +0900

    add to do list

---
+++ to-do-list/index.js
@@ -0,0 +1,17 @@
+const date = document.querySelector(".date")
+const checkbox = document.querySelector(".checkbox");
+const checkList = document.querySelector(".check-list");
+const listSpace = document.querySelector(".list-space");
+const addBtn = document.querySelector(".add-btn");
+
+/**
+ * 기능
+ * 할일 추가하기 눌렀을때 체크리스트 추가
+ * 체크박스 눌렀을때 할일 지우기 (가운데 선)
+ */
+
+addBtn.addEventListener("click", () => {
+  const input = createElement("input");
+  input.classList.add()
+
+})
