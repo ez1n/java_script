4f0d9954c75350041b6a54346afe9625f58bebe0
unknown <eodrmfdl1004@naver.com>
2022-03-01 21:43:40 +0900

    add checkbox - input box

--- to-do-list/index.js
+++ to-do-list/index.js
@@ -6,12 +6,23 @@ const addBtn = document.querySelector(".add-btn");
 
 /**
  * 기능
- * 할일 추가하기 눌렀을때 체크리스트 추가
  * 체크박스 눌렀을때 할일 지우기 (가운데 선)
  */
 
 addBtn.addEventListener("click", () => {
-  const input = createElement("input");
-  input.classList.add()
+  const li = document.createElement("li");
+  const textInput = document.createElement("input");
+  const checkInput = document.createElement("input");
+  textInput.type = "text";
+  checkInput.type = "checkbox";
+  li.className = "list-input";
+  textInput.className = "check-list";
+  checkInput.className = "checkbox";
 
-})
+  li.append(checkInput, ' ',textInput);
+  listSpace.appendChild(li);
+});
+
+checkbox.addEventListener("click", () => {
+
+});
